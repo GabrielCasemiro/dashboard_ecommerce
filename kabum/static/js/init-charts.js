88efc603bd47746b5f9384ec028967f08520af16
Gabriel <gabrielcasemiro68@gmail.com>
2020-03-25 15:37:29 -0300

    Inserção de dados e gráficos

---
+++ kabum/static/js/init-charts.js
@@ -0,0 +1,51 @@
+function createSaleChart(graph_data_sales, graph_data_ecommerce, labels, year) {
+    if ($('#SalesChart').length) {
+        var ctx = document.getElementById("SalesChart").getContext('2d'),
+            gradient = ctx.createLinearGradient(0, 0, 0, 400),
+            gradient2 = ctx.createLinearGradient(0, 0, 0, 400);
+        gradient.addColorStop(1, 'rgba(44,173,245,0.1)');
+        gradient.addColorStop(0, 'rgba(79,203,251, 1)');
+        gradient2.addColorStop(1, 'rgba(154,119,96,0.1)');
+        gradient2.addColorStop(0, 'rgba(248,207,105,1)');;
+        ctx.height = 150;
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Sales in "+ year +" (Million Dolars)",
+                        borderColor: gradient2,
+                        borderWidth: "1",
+                        backgroundColor: gradient2,
+                        pointHighlightStroke: "rgba(245, 23, 66,.5)",
+                        data: graph_data_sales
+                    },
+                    {
+                        label: "E-commerce sales in "+ year +" (Million Dolars)",
+                        borderColor: gradient,
+                        borderWidth: "1",
+                        backgroundColor: gradient,
+                        data: graph_data_ecommerce
+                    }
+
+
+                ]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                }
+
+            }
+        });
+    }
+}
+createSaleChart();
+
---
+++ kabum/static/js/script.js
@@ -0,0 +1,62 @@
+function populateGraph(year, graph_data_sales, graph_data_ecommerce, labels) {
+    if (year <= 2015 && year >= 1999) {
+        //Se o ano for válido
+        var min_year = year - 3;
+        var max_year = year + 3;
+    } else {
+        return
+    }
+
+    for (i = max_year; i >= year; i--) {
+        try {
+            if (labels.includes(i) == false && i != year) {
+                graph_data_sales.push(dados[i]["Total_sales"]);
+                graph_data_ecommerce.push(dados[i]["Total_Ecommerce"]);
+                labels.push(i);
+            }
+
+        } catch (err) {
+
+        }
+
+    }
+    //Dados do ano no gráfico
+    graph_data_sales.push(dados[year]["Total_sales"]);
+    graph_data_ecommerce.push(dados[year]["Total_Ecommerce"]);
+    labels.push(year);
+
+    for (i = year; i >= min_year; i--) {
+        try {
+            if (labels.includes(i) == false && i != year) {
+                graph_data_sales.push(dados[i]["Total_sales"]);
+                graph_data_ecommerce.push(dados[i]["Total_Ecommerce"]);
+                labels.push(i);
+            }
+
+        } catch (err) {
+
+        }
+    }
+    return graph_data_sales, graph_data_ecommerce, labels
+}
+function updateTable(labels, dados, year) {
+    $("#tbody").html("");
+    //Percorre todas as linhas
+    for(i=0;i<labels.length;i++){
+        if(dados[year]["Ecommerce"][i] == 0){
+            dado_ecommerce = 'S';
+            
+        }else{
+            dado_ecommerce = "$ " + dados[year]["Ecommerce"][i] + " M";
+        }
+        if(dados[year]["Sales"][i] == 0){
+            dado_sales = 'S';
+            
+        }else{
+            dado_sales = "$ " + dados[year]["Sales"][i] + " M";
+        }
+        $("#tbody").append('<tr> <td>'+labels[i]+'</td> <td>'+ dado_sales +'</td> <td>'+dado_ecommerce+'</td> </tr>')
+        console.log( + ": " )
+    }
+}
+
